e35a3627a07ecace6a71ab3b9b6a8f9d6bc20aed
saif8364 <saifullah7868364@gmail.com>
2024-12-30 13:48:24 -0800

    hm variable name updated

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -5,7 +5,7 @@ class apiError extends Error{
         errors=[],
         stack=""
     ){
-           super(msg);
+           super(message);
            this.statusCode=statusCode;
            this.data=null;
            this.message=message;
